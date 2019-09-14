84f59bf4f7b53beb399fd31554fd6d39da3c9bea
Eddy.Kim <sieunkr@gmail.com>
2019-09-14 21:48:29 +0900

    논블록킹 코드 검증

--- completablefuture/src/test/java/com/example/demo/CoffeeComponentTest.java
+++ completablefuture/src/test/java/com/example/demo/CoffeeComponentTest.java
@@ -55,12 +55,22 @@ public class CoffeeComponentTest {
     @Test
     public void 가격_조회_비동기_호출_콜백_반환없음_테스트(){
 
+        Integer expectedPrice = 1100;
+
         CompletableFuture<Void> future = coffeeComponent
                 .getPriceAsync("latte")
-                .thenAccept(p -> log.info("콜백, 가격은 " + p + "원, 하지만 데이터를 반환하지는 않음"));
+                .thenAccept(p -> {
+                    log.info("콜백, 가격은 " + p + "원, 하지만 데이터를 반환하지는 않음");
+                    assertEquals(expectedPrice, p);
+                });
 
-        log.info("아직 최종 데이터를 전달 받지는 않았지만, 다른 작업 수행 가능");
+        log.info("아직 최종 데이터를 전달 받지는 않았지만, 다른 작업 수행 가능, 논블록킹");
 
+        /*
+        아래 구문이 없으면, main thread 가 종료되기 때문에, thenAccept 확인하기 전에 끝나버림.
+        그래서, 테스트를 위해서 메인쓰레드가 종료되지 않도록 블록킹으로 대기하기 위한 코드
+        future 가 complete 가 되면 위에 작성한 thenAccept 코드가 실행이 됨
+        */
         assertNull(future.join());
     }
 
@@ -80,7 +90,7 @@ public class CoffeeComponentTest {
         log.info("아직 최종 데이터를 전달 받지는 않았지만, 다른 작업 수행 가능");
 
         int resultPrice = future.join();  //블록킹
-        log.info("최종 가격 전달 받음");
+
 
         assertEquals(expectedPrice, resultPrice);
     }
